328e0a28feb4a5f88f4812f376511171dcf6af43
valendc15 <valentino.dicapua@ing.austral.edu.ar>
2024-05-30 01:14:39 -0300

    fix: fixed domain and clienId access through env

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import {PaginationProvider} from "./contexts/paginationProvider.tsx";
 import {SnackbarProvider} from "./contexts/snackbarProvider.tsx";
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain=process.env.REACT_APP_AUTH0_DOMAIN as string
-const clientId=process.env.REACT_APP_AUTH0_CLIENT_ID as string
+const domain=import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN as string
+const clientId=import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID as string
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
