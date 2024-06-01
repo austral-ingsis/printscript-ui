df878563c839d4ca56c705e768f197cc24543254
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-06-01 19:44:07 -0300

    Login, Logout and Profile info

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,21 @@ import './index.css'
 import {createRoot} from "react-dom/client";
 import {PaginationProvider} from "./contexts/paginationProvider.tsx";
 import {SnackbarProvider} from "./contexts/snackbarProvider.tsx";
+import {Auth0Provider} from "@auth0/auth0-react";
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <PaginationProvider>
         <SnackbarProvider>
-          <App/>
+            <Auth0Provider
+                domain="dev-n5rdb5xcsb4ya1cg.us.auth0.com"
+                clientId="lCrzgtui3ckfodVyLn2ELx54bsSbIgs4"
+                authorizationParams={{
+                    redirect_uri: window.location.origin
+                }}
+            >
+                <App/>
+            </Auth0Provider>
         </SnackbarProvider>
       </PaginationProvider>
     </React.StrictMode>,
