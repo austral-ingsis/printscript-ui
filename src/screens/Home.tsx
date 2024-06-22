a64c3e5b96e07476aa1e91b843bcd342a94b0688
valendc15 <valentino.dicapua@ing.austral.edu.ar>
2024-06-22 19:27:07 -0300

    feat: added simple request to get snippets

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,26 +4,47 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { SnippetDetail } from "./SnippetDetail.tsx";
 import { Drawer } from "@mui/material";
-import { useGetSnippets } from "../utils/queries.tsx";
 import { usePaginationContext } from "../contexts/paginationContext.tsx";
 import useDebounce from "../hooks/useDebounce.ts";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 const HomeScreen = () => {
   const { id: paramsId } = useParams<{ id: string }>();
   const [searchTerm, setSearchTerm] = useState("");
-  // const [token, setToken] = useState<any>("");
   const [snippetName, setSnippetName] = useState("");
   const [snippetId, setSnippetId] = useState<string | null>(null);
   const { page, page_size, count, handleChangeCount } = usePaginationContext();
-  const { data, isLoading } = useGetSnippets(page, page_size, snippetName);
-  // const { user, getAccessTokenSilently, getIdTokenClaims} = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
+  const [data, setData] = useState<{ snippets: any[]; count: number } | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (data?.count && data.count != count) {
+    const fetchSnippets = async () => {
+      setIsLoading(true);
+      try {
+        const token = await getAccessTokenSilently();
+        const response = await axios.get(`http://localhost:8081/snippet`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching snippets", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchSnippets();
+  }, [page, page_size, snippetName, getAccessTokenSilently]);
+
+  useEffect(() => {
+    if (data?.count && data.count !== count) {
       handleChangeCount(data.count);
     }
-    // getIdTokenClaims().then((token) => setToken(token));
-  }, [count, data?.count, handleChangeCount]);
+  }, [data?.count, count, handleChangeCount]);
 
   useEffect(() => {
     if (paramsId) {
@@ -33,7 +54,6 @@ const HomeScreen = () => {
 
   const handleCloseModal = () => setSnippetId(null);
 
-  // DeBounce Function
   useDebounce(
     () => {
       setSnippetName(searchTerm);
